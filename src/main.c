ecb656ecacbb8be5082344e6c28ffc7c030ce8e3
dodgez <zacharysdodge@gmail.com>
2019-12-13 21:46:15 -0800

    Add capability to run file(s) before repl

--- src/main.c
+++ src/main.c
@@ -1,5 +1,38 @@
 #include "main.h"
 
-int main(void) {
+int main(int argc, char** argv) {
+  variables = (Var*)malloc(sizeof(Var) * 1);
+  variable_count = 0;
+  allocated_variables = 1;
+
+  if (argc > 1) {
+    char* line = NULL;
+    FILE* file = NULL;
+    Node* node = NULL;
+    size_t length = 0;
+    size_t read = 0;
+    for (int i = 1; i < argc; ++i) {
+      file = fopen(argv[i], "r");
+      if (file == NULL) {
+        printf("Error: could not open file '%s'\n", argv[i]);
+      }
+
+      while ((read = getline(&line, &length, file)) != -1) {
+        node = parseExpression(getStreamFromString(line));
+        if (node == NULL) {
+          printf(error_message);
+        } else {
+          Value* result = eval(node);
+          printf("Variables defined: %d\n", variable_count);
+        }
+      }
+      fclose(file);
+    }
+    if (line) {
+      free(line);
+    }
+  }
   repl();
+
+  return 0;
 }
--- src/repl.c
+++ src/repl.c
@@ -3,10 +3,7 @@
 void repl(void) {
   Node* node;
   char* input = NULL;
-  size_t size;
-  variables = (Var*)malloc(sizeof(Var) * 1);
-  variable_count = 0;
-  allocated_variables = 1;
+  size_t size = 0;
   while (TRUE) {
     printf(">");
     getline(&input, &size, stdin);
@@ -20,10 +17,6 @@ void repl(void) {
       }
       if (result->type == NUMBER_VALUE) {
         printf("%g\n", ((double*)result->data)[0]);
-      }
-
-      for (int i = 0; i < variable_count; ++i) {
-        printf("Variable '%s' with value type %d\n", variables[i].name, variables[i].data->type);
       }
     }
   }
